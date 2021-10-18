9fe700ff25fffafa458bee8e691b1ddcbeb8656e
Rajakrishnan <rajakrishnanrvs@gmail.com>
2021-10-18 11:53:58 +0530

    1.🖇🔗 linking the catalog app's url with the project url file. 2.➕adding the STATIC🤚 URL for development👩‍💻 in project's url file

---
+++ catalog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .import views import
+
+app_name = 'catalog' # to avoid namesapace collision from other apps, while referring in the project scope
+
+url_patterns = [
+    
+]
