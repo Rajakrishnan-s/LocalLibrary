d3309789c14866ead8078f88888a02789eb4d7f3
Rajakrishnan <rajakrishnanrvs@gmail.com>
2021-10-18 12:57:28 +0530

    Added➕ gitignore🤷‍♂️ file

--- locallibrary/asgi.py
+++ locallibrary/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Tutorial_project project.
+ASGI config for locallibrary project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tutorial_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'locallibrary.settings')
 
 application = get_asgi_application()
