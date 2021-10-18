ed6516f32719e2934fa537eee9dd89024d6684c5
Rajakrishnan <rajakrishnanrvs@gmail.com>
2021-10-18 12:57:28 +0530

    Added➕ gitignore🤷‍♂️ file

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .import views import
+from . import views
 
 app_name = 'catalog' # to avoid namesapace collision from other apps, while referring in the project scope
 
 url_patterns = [
-    
+
 ]
