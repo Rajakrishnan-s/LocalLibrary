044a10186b2bb04b529120e14310a9a0db2a0227
Rajakrishnan <rajakrishnanrvs@gmail.com>
2021-10-22 13:58:56 +0530

    🔧🛠 Repair again, Urls & views.py files updated

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 app_name = 'catalog' # to avoid namesapace collision from other apps, while referring in the project scope
 
-url_patterns = [
-
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('authors/', views.AuthorListView.as_view(), name='authors')
 ]
