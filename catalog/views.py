7bebbeea9063ea1d17da2f144831826cb5410e95
Rajakrishnan <rajakrishnanrvs@gmail.com>
2021-10-27 21:06:58 +0530

    Linked the BookInstance model with User model to provide Borrowed books functionality for the user,
    And created the borrowed book list view component

--- catalog/views.py
+++ catalog/views.py
@@ -42,3 +42,12 @@ class BookDetailView(LoginRequiredMixin, generic.DetailView):
 
 class AuthorDetailView(LoginRequiredMixin, generic.DetailView):
     model = Author
+
+class LoanedBooksByUserListView(LoginRequiredMixin,generic. ListView):
+    """Generic class-based view listing books on loan to current user."""
+    model = BookInstance
+    template_name ='catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
