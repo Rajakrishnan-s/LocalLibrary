2feb07386c33d97dd2a56768df82fbef356dd986
Rajakrishnan <rajakrishnanrvs@gmail.com>
2021-10-31 10:29:52 +0530

    1.created view components, where only users with "can_mark_returned" permission can see the list of all the currently borrowed books. Note: In the current version, admin can create a user group, and assign any permission to it.

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,8 +6,9 @@ app_name = 'catalog' # to avoid namesapace collision from other apps, while refe
 urlpatterns = [
     path('', views.index, name='index'),
     path('books/', views.BookListView.as_view(), name='books'),
-    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book_detail'),
     path('authors/', views.AuthorListView.as_view(), name='authors'),
-    path('authors/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
-    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path('authors/<int:pk>', views.AuthorDetailView.as_view(), name='author_detail'),
+    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my_borrowed'),
+    path('allborrowed/', views.AllBorrowedBooksListView.as_view(), name='all_borrowed')
 ]
